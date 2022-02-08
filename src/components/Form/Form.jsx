7fb7be3d6fcc990b91a012e6ce5435de4e6ac921
Emma Egstad <emma@egstad.com>
2022-02-07 19:35:58 -0800

    adding context file

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,7 +3,7 @@ import './Form.css';
 function Form() {
   return (
     <form className="Form">
-      <p className="form-text">Sign here!</p>
+      <h2 className="form-text">Sign here!</h2>
       <label>Guest Name</label>
       <input type="text"></input>
       <label>Guest Message</label>
---
+++ src/context/UserContext/UserContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useMemo, useState } from 'react';
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState('');
+
+  const value = { user, setUser };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+
+  return context;
+};
+
+export { UserProvider, useUser };
